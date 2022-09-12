38203f7e01ca170840862083568049edd48058f0
Martin Knab <martin.knab@student.ibs-ol.de>
2022-09-12 19:14:06 +0200

    html files

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
-if __name__ == '__main__':
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/imprint")
+def imprint():
+    return render_template("imprint.html")
+
+
+if __name__ == "__main__":
     app.run()
